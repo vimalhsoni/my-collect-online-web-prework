4e9deb7dc94a3f373229b10dc3c79c1e04f15fd0
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-10 22:09:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,9 @@ def my_collect(collection)
   i = 0
   new_array = Array.new
   while i < collection.size
-    
+
     binding.pry
-    
+
     yield(collection[i])
     i = i + 1
     new_array.push
