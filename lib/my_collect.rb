4438a955dc25c25bc2b15433318543c42df0ee1f
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-10 22:43:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+#require 'pry'
 
-
+def my_collect(collection)
+  i = 0
+  new_array = Array.new
+  while i < collection.size
+    new_array.push(yield(collection[i]))
+    i = i + 1
+  end
+  return new_array
+end
