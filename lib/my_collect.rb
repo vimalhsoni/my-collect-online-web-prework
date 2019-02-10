927b081e515c3e134601564287082105c07ced04
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-10 22:26:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,10 @@
-require 'pry'
+#require 'pry'
 
 def my_collect(collection)
   i = 0
   new_array = Array.new
   while i < collection.size
     yield(collection[i])
-
-    binding.pry
-    
     i = i + 1
     new_array.push
   end
