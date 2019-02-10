c0b1fbd7ae161dbcf252fe7327dd13d742b42198
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-10 20:54:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
 
-def my_collect(empty_array)
-  i = 0
-  while i < empty_array.length
-    yield(empty_array[i])
-    i = i + 1
-    return empty_array
+def(VALUE ary)
+  long i;
+  VALUE collect;
+
+  RETURN_SIZED_ENUMERATOR(ary, 0, 0, ary_enum_length);
+  collect = rb_ary_new2(RARRAY_LEN(ary));
+  for (i = 0; i < RARRAY_LEN(ary); i++) {
+    rb_ary_push(collect, rb_yield(RARRAY_AREF(ary, i)));
   end
+  return collect;
 end
